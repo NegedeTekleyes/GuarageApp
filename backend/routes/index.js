8e13347ea37a488c8336882c180649b27d8ddd56
Negede <negedetekleyes@gmail.com>
2026-03-16 00:55:09 +0300

    handles install route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,4 +6,4 @@ const installRouter = require('./install.routes')
 const { router } = require('../app')
           // Add the install router to the main router 
 router.use(installRouter);
-module.exports(router)
+module.exports= router;
