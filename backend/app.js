bdad8e1dc1686b6ac0f8e7b53d569e27ea4a824c
Negede <negedetekleyes@gmail.com>
2026-03-15 22:08:42 +0300

    create webserve

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+// 
+const express = require('express')
+// import env and call config file to load it
+require('dotenv').config();
+// Create a variable to hold our port number 
+const port = process.env.PORT
+// Start webserver
+app.listen(port, () => {
+console.log(`Server running on Port!", ${port}`)
+})
+// module export
+module.exports = app;
