f5b6fd6a25d52a2cf1900cd8eb13ff63fca21e89
Negede <negedetekleyes@gmail.com>
2026-03-15 22:39:57 +0300

    update server running

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,10 @@ const express = require('express')
 require('dotenv').config();
 // Create a variable to hold our port number 
 const port = process.env.PORT
+const app = express()
 // Start webserver
 app.listen(port, () => {
-console.log(`Server running on Port!", ${port}`)
+console.log(`Server running on Port!" ${port}`)
 })
 // module export
 module.exports = app;
