a29b8961e4a089ae75f706577d32a2509bae6415
Negede <negedetekleyes@gmail.com>
2026-03-17 01:50:55 +0300

    routes added

--- frontend/Gurage App/src/App.jsx
+++ frontend/Gurage App/src/App.jsx
@@ -1,18 +1,30 @@
 
 import { Route, Routes } from 'react-router'
-import './App.css'
+// import './App.css'
 import Login from './markup/pages/Login'
 import Home from './markup/pages/Home'
 import AddEmployee from './markup/pages/admin/add-employee'
+import Footer from './markup/components/Footer/Footer'
+// Import the css files 
+import "./assets/template_assets/css/bootstrap.css";
+import "./assets/template_assets/css/style.css";
+import "./assets/template_assets/css/responsive.css";
+import "./assets/template_assets/css/color.css";
+import "./assets/styles/custome.css"
+import Header from './markup/components/Header/Header'
+
 function App() {
+
 
   return (
     <>
+  <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/admin/add-employee' element={<AddEmployee/>}/>
     </Routes>
+    <Footer/>
     </>
   )
 }
