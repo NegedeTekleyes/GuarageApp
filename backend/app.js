808dda9b58a8c1358e879a38d1c1c4a74fdefff6
Negede <negedetekleyes@gmail.com>
2026-03-16 21:08:05 +0300

    add routes to app js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
-// 
+
 const express = require('express')
 // import env and call config file to load it
 require('dotenv').config();
 // Create a variable to hold our port number 
+const router = require('./routes/install.routes')
 const port = process.env.PORT
 const app = express()
+app.use(router);
 // Start webserver
 app.listen(port, () => {
 console.log(`Server running on Port!" ${port}`)
