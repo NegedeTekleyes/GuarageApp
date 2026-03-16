77374d2ffae8960bba4ae5d5b95feb98bc20e264
Negede <negedetekleyes@gmail.com>
2026-03-16 18:52:16 +0300

    update config using postgre db

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,19 +1,21 @@
-// imort mysql modelw
-const mysql = require('mysql2/promise')
+// import posgress modules
+const {pool} =  require('pg');
+
 // prepare connection parameter
 const dbConfig = {
-     connectionLimit: 10,
-     password: process.env.DB_PASS,
-     user: process.env.DB_USER,
-     host: process.env.DB_HPOST,
-     database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 5432,
+    password: process.env.DB_PASS,
 }
+// create connection pool
+const pool = new Pool(dbConfig);
 
-// create the connection
-const pool = mysql.createPool(dbConfig)
-
-// create a function that excute sql quries asynchorously
-async function query(sql, params) {
-    const [rows, fields] = await pool.execute(sql, params)
-    return rows;
+// create a function excutes sql queries asynchronously
+async function query(sql,params) {
+    const res = await pool.query(sql,params)
+    return res.rows;
+    
 }
+module.exports = {query}
