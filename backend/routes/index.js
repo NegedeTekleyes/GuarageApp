4dc08d1418a0738bf130384e538c151f67eb8b55
Negede <negedetekleyes@gmail.com>
2026-03-17 22:28:38 +0300

    employee route import

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,10 @@
-// 
-const express = require('express')
-const route = express.Router()
 
-const installRouter = require('./install.routes')
-const { router } = require('../app')
-          // Add the install router to the main router 
-router.use(installRouter);
+const express = require('express')
+const router = express.Router()
+
+const installRouter = require('./install.routes');
+//           // Add the install router to the main router 
+const employeeRouter = require('./employee.routes')
+router.use('/install', installRouter);
+router.use('/api/employee', employeeRouter)
 module.exports= router;
