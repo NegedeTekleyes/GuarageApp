15c8b40d47245a43d31f81b959af93fba5eb10ac
Negede <negedetekleyes@gmail.com>
2026-03-18 11:53:43 +0300

    routes added

---
+++ backend/routes/aiRoute.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+// const router = express.Router()
+// // const askAI = require('../ai/chatbot')
+
+// router.post('/chat', async(req, res) => {
+//     try {
+//         const {message} = req.body
+//         const reply = await askAI(message)
+//         res.json({reply})
+//     } catch (error) {
+//         res.status(500).json({error: "AI error"})
+//     }
+// })
+// module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,6 +5,8 @@ const router = express.Router()
 const installRouter = require('./install.routes');
 //           // Add the install router to the main router 
 const employeeRouter = require('./employee.routes')
+const loginRouter = require('./login.routes')
 router.use('/install', installRouter);
 router.use('/api/employee', employeeRouter)
+router.use('/api/employee', loginRouter)
 module.exports= router;
