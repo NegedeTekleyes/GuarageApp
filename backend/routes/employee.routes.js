f7279ca5d686e05f8851b4750990bf46aac525a3
Negede <negedetekleyes@gmail.com>
2026-03-18 10:19:25 +0300

    update routes

---
+++ backend/routes/employee.routes.js
@@ -0,0 +1,10 @@
+
+const express = require('express')
+const router = express.Router()
+
+
+const employeeController = require('../controllers/employee.controller')
+// create a route to handle controller
+router.post("/", employeeController.createEmployee)
+
+module.exports = router
