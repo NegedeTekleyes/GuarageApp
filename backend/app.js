21ededff64088a811ec54601a544984aaedde604
Negede <negedetekleyes@gmail.com>
2026-03-25 23:48:58 +0300

    route order update

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,31 @@
 
 const express = require('express')
+const app = express()
 // import env and call config file to load it
 require('dotenv').config();
 // Import the sanitizer module 
-const sanitize = require('sanitize');
-// import cors
 const cors = require('cors')
+const sanitize = require('sanitize');
+
 
 const corsOption = {
     origin: process.env.FRONTEND_URL,
-    optionsSucessStatus: 200,
+    optionsSuccessStatus: 200,
 }
+app.use(cors(corsOption))
+// app.options(cors(corsOption))
+app.use(express.json())
+app.use(sanitize.middleware)
 // Create a variable to hold our port number 
 const router = require('./routes')
+app.use(router)
 // const employeeRoute = require('../backend/routes/employee.routes')
 // const loginRoute = require('./routes/login.routes')
 const port = process.env.PORT
-const app = express()
-app.use(express.json())
-app.use(router)
 // app.use('/api/employee/login', loginRoute)
         //  We will need to add it on our app as a middleware 
 
-app.use(sanitize.middleware)
-app.use(cors(corsOption))
 // add the routes as a medileware b/4 the webserver start
-app.use(router);
 // const aiRoute = require('./routes/aiRoute')
 // app.use('api/ai', aiRoute)
 // Start webserver
