b14571f4b225207167f32467a29050d47d6be129
Negede <negedetekleyes@gmail.com>
2026-03-26 11:39:59 +0300

    add form handler and states to hold values

--- frontend/Gurage App/src/markup/components/LoginForm/LoginForm.jsx
+++ frontend/Gurage App/src/markup/components/LoginForm/LoginForm.jsx
@@ -1,5 +1,58 @@
+import { useState } from "react"
+import {useNavigate,useLocation} from "react-router-dom"
 
 function LoginForm() {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const [employee_email, setEmail] = useState('')
+  const [employee_password, setPassword] = useState('')
+  const [emailError, setEmailError] = useState('')
+  const [passwordError, setPasswordError] = useState('')
+  const [serverError, setServerError] = useState('')
+
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+
+    let valid = true
+    // email is required
+    if(!employee_email) {
+      setEmail('Please Enter Email address first')
+    } else if (!employee_email.includes ('@')){
+      setEmailError('Invalid Email format')
+     
+    } else {
+      const regex = /^\S+@\S+\.\S+$/;
+      if(!regex.test(employee_email)) {
+        setEmail('Invalid Email format')
+        valid = false
+      }
+      setEmailError('')
+       valid = false
+    }
+    // password is required
+    if(!employee_password || employee_password.length < 6) {
+      setPassword('Password length is must be at least  6 character long.')
+    } else{
+      setPassword('')
+      valid = false
+    }
+
+    const formData = {
+      employee_email,
+      employee_password
+    }
+
+    const loginEmployee = loginService.logIn(formData)
+    loginEmployee.then((response) => response.json())
+    .then((response) => {
+      if (response.status = "sucess"){
+        // save the user in the local storage
+        if (response.data.employee_token) {
+          localStorage.setItem("employee", JSON.stringify(response.data))
+        }
+      }
+    })
+  }
   return (
     <section className="contact-section">
       <div className="auto-container">
@@ -10,10 +63,15 @@ function LoginForm() {
           <div className="form-column col-lg-7">
             <div className="inner-column">
               <div className="contact-form">
-                <form>
+                <form onSubmit={handleSubmit}>
                   <div className="row clearfix">
                     <div className="form-group col-md-12">
-                      <input type="email" name="employee_email" placeholder="Email" />
+                      {serverError && <div className="validation" role="alert">{serverError}</div>
+                      }
+                      <input type="email" value={employee_email} name="employee_email" onChange={event => setEmail(event.target.value)} placeholder="Email" />
+                      {emailError &&
+                      <div className="validation-error" role="alert">{emailError}</div>
+                      }
                     </div>
                     <div className="form-group col-md-12">
                       <input type="password" name="employee_password" placeholder="Password" />
