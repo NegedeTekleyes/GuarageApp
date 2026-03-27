6b05dfb1e3926eca6d0252461c4a73dbdcbc9dba
Negede <negedetekleyes@gmail.com>
2026-03-27 13:35:56 +0300

    auth provider adde

--- frontend/Gurage App/src/main.jsx
+++ frontend/Gurage App/src/main.jsx
@@ -6,7 +6,10 @@ import {BrowserRouter} from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <AuthProvider>
       <App />
+
+    </AuthProvider>
     
     </BrowserRouter>
   </StrictMode>,
